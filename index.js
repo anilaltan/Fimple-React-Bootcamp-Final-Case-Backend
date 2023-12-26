e974f015d9cf1aef7b12dcfeb97719be44557a72
Anil Altan <anlaltnn@gmail.com>
2023-12-26 17:28:39 +0300

    adding cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const ticketRoutes = require("./routes/ticket");
 const userRoutes = require("./routes/user");
 const port = process.env.PORT;
@@ -6,6 +7,7 @@ require("./db/db");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(userRoutes);
 app.use(ticketRoutes);
