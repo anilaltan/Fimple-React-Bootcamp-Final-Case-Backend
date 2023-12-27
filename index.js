72379f54c449e6ad151fa0aab5c6550e82728f6e
Anil Altan <anlaltnn@gmail.com>
2023-12-27 22:14:03 +0300

    Update cors

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ require("./db/db");
 
 const app = express();
 
-var whitelist = ["http://localhost:5173", "http://127.0.0.1:5173"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// var whitelist = ["http://localhost:5173", "http://127.0.0.1:5173"];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions)); // define this middeware before the all routes as you defined.
+// app.use(cors(corsOptions)); // define this middeware before the all routes as you defined.
+app.use(cors());
+
 app.use(express.json());
 app.use(userRoutes);
 app.use(ticketRoutes);
